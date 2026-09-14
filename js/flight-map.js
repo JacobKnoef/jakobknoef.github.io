93dd3101cf6c1aa3b6d7f1c1701eba74bb709ab4
JacobKnoef <139674885+JacobKnoef@users.noreply.github.com>
2026-09-14 21:04:39 +1200

    Add flight map page

---
+++ js/flight-map.js
@@ -0,0 +1,8 @@
+const map = L.map("flight-map").setView([-20, 20], 2);
+
+L.tileLayer(
+    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+    {
+        attribution: "&copy; OpenStreetMap contributors"
+    }
+).addTo(map);
